0268dd0b36065dc271b619c51c1906c9e2e39223
Aras <arkadiuszchr@gmail.com>
2017-11-20 18:29:49 +0100

    xunit tests

---
+++ tests/Evento.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Evento.Core.Domain;
+using Evento.Core.Repositories;
+using Evento.Infrastructure.DTO;
+using Evento.Infrastructure.Services;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Evento.Tests.Services
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async Task register_async_should_invoke_add_async_on_user_repository()
+        {
+            //Arrange
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var jwtHandler = new Mock<IJwtHandler>();
+            var mapperMock = new Mock<IMapper>();
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object, jwtHandler.Object);
+
+            //Act
+            await userService.RegisterAsync(Guid.NewGuid(), "test@test.com", "test", "secret");
+
+            //Assert
+            userRepositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task when_invoking_get_async_it_should_invoke_get_async_on_user_repository()
+        {
+            //Arrange
+            var user = new User(Guid.NewGuid(), "user", "test", "test@test.com", "secret");
+            var accountDto = new AccountDTO
+            {
+                Id = user.Id,
+                Role = user.Role,
+                Email = user.Email,
+                Name = user.Name
+            };
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var jwtHandler = new Mock<IJwtHandler>();
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(x => x.Map<AccountDTO>(user)).Returns(accountDto);
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object, jwtHandler.Object);
+
+            userRepositoryMock.Setup(x => x.GetAsync(user.Id)).ReturnsAsync(user);
+
+            //Act
+            var existingAccountDto = await userService.GetAccountAsync(user.Id);
+
+            //Assert
+            userRepositoryMock.Verify(x => x.GetAsync(user.Id), Times.Once());
+            accountDto.Should().NotBeNull();
+            accountDto.Email.ShouldAllBeEquivalentTo(user.Email);
+        }
+    }
+}
